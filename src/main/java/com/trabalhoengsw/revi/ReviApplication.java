a3e03caa03230e33602489ed5b7949c576cf6169
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-10-23 09:17:19 -0300

    repo started

---
+++ src/main/java/com/trabalhoengsw/revi/ReviApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhoengsw.revi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviApplication.class, args);
+	}
+
+}
